d9e7b5a951ff58168f7222ee67cbb2ee0f8eeb8c
Jonathan Whitlock <beenencoded@outlook.com>
2020-01-25 22:16:14 -0500

    work on the ui

--- src/UI/widgets.py
+++ src/UI/widgets.py
@@ -1,11 +1,19 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton, QLabel, QPlainTextEdit
+from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
+
+from quithread import ThreadData, WindowUpdateThread
+
+import logging, threading, io
+
+logger = logging.getLogger("widgets")
 
 class MainBuildMenu(QWidget):
     def __init__(self, parent):
         super(MainBuildMenu, self).__init__(parent)
 
         self._init_layout()
+        self._connect_handlers()
     
     def _init_layout(self):
         mainlayout = QVBoxLayout()
@@ -15,3 +23,40 @@ class MainBuildMenu(QWidget):
 
         mainlayout.addWidget(self.l)
         self.setLayout(mainlayout)
+    
+    def _connect_handlers(self):
+        pass
+
+class UIStream(QtCore.QObject):
+    '''
+    Can be used to redirect stdout and stderr from the console to 
+    a UI element.
+    '''
+
+    _stdout = None
+    _stderr = None
+    messageWritten = pyqtSignal(str)
+
+    def flush( self ):
+        pass
+    
+    def fileno( self ):
+        return -1
+    
+    def write( self, msg ):
+        if ( not self.signalsBlocked() ):
+            self.messageWritten.emit(unicode(msg))
+
+    @staticmethod
+    def stdout():
+        if ( not XStream._stdout ):
+            XStream._stdout = XStream()
+            sys.stdout = XStream._stdout
+        return XStream._stdout
+
+    @staticmethod
+    def stderr():
+        if ( not XStream._stderr ):
+            XStream._stderr = XStream()
+            sys.stderr = XStream._stderr
+        return XStream._stderr

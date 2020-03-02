7ba2a722da869082a30541c05a642e8ff2d5df51
Jonathan Whitlock <beenencoded@outlook.com>
2020-03-02 17:52:58 -0500

    testing projectInformation

--- src/UI/widgets.py
+++ src/UI/widgets.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import Qt, pyqtSlot
 import logging, threads
 
 from UI.stdredirect import STDOutWidget
+from data import ProjectInformation, CMAKE_GENERATOR_TYPES
 
 logger = logging.getLogger(__name__)
 
@@ -20,16 +21,14 @@ class MainBuildMenu(QWidget):
 
     def _init_layout(self):
         mainlayout = QVBoxLayout()
-
-        self.l = QLabel("Hello World")
-        self.l.setAlignment(Qt.AlignCenter)
-
-        mainlayout.addWidget(self.l)
+        self.test_button = QPushButton("Test")
+        
+        mainlayout.addWidget(self.test_button)
         mainlayout.addWidget(STDOutWidget(self))
         self.setLayout(mainlayout)
     
     def _connect_handlers(self):
-        pass
+        self.test_button.clicked.connect(self.dataTest)
 
     @pyqtSlot()
     def printtest(self) -> None:
@@ -37,3 +36,19 @@ class MainBuildMenu(QWidget):
         print("Print test message\nmultiline test")
         print("a seperate print line")
 
+    @pyqtSlot()
+    def dataTest(self) -> None:
+        testinfo = ProjectInformation()
+        testinfo.project_directory = "C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019"
+        testinfo.source_directory = "C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019\\src"
+        testinfo.cpp_compiler = "C:\\LLVM\\bin\\clang++.exe"
+        testinfo.c_compiler = "C:\\LLVM\\bin\\clang.exe"
+        testinfo.generator_type = CMAKE_GENERATOR_TYPES[14]
+        testinfo.make_cmd = "nmake"
+        testinfo.build_targets = ["release", "debug"]
+        testinfo.cmake_include_path = ["C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019\\src\\main"]
+        testinfo.cmake_library_path = ["C:\\Users\\lib",
+                                        "C:\\LLVM\\lib\\clang\\9.0.0\\include"]
+        logger.debug("Testinfo: ")
+        logger.debug(testinfo.cmake())
+        logger.debug(testinfo.make())

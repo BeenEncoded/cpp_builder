23361267d16fce4c8873e0a536b45fdc01197a75
Jonathan Whitlock <beenencoded@outlook.com>
2020-03-04 12:53:34 -0500

    switched to raw strings;

--- src/unit_tests/testdata.py
+++ src/unit_tests/testdata.py
@@ -1,22 +1,21 @@
 import logging
 
-from data import ProjectInformation, CMAKE_GENERATOR_TYPES
+from data import ProjectInformation, SupportedCmakeGenerators
 
 logger = logging.getLogger("TEST: " + __name__)
 
 def actual_project_information() -> ProjectInformation:
     testinfo = ProjectInformation()
-    testinfo.project_directory = "C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019"
-    testinfo.source_directory = "C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019"
-    testinfo.build_directory = "C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019\\build"
-    testinfo.cpp_compiler = "C:\\LLVM\\bin\\clang++.exe"
-    testinfo.c_compiler = "C:\\LLVM\\bin\\clang.exe"
-    testinfo.generator_type = CMAKE_GENERATOR_TYPES[14]
-    testinfo.make_cmd = "nmake.exe"
+    testinfo.project_directory = r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019"
+    testinfo.source_directory = r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019"
+    testinfo.build_directory = r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019\build"
+    testinfo.cpp_compiler = r"C:\LLVM\bin\clang++.exe"
+    testinfo.c_compiler = r"C:\LLVM\bin\clang.exe"
+    testinfo.generator_type = SupportedCmakeGenerators.NMAKE_MAKEFILE
     testinfo.cmake_cmd = "cmake.exe"
     testinfo.build_targets = ["debug"]
-    testinfo.cmake_include_path = ["C:\\Users\\beene.DESKTOP-NGBJDSG\\Documents\\coding\\C++\\Current_projects\\test_2019\\src\\main"]
-    testinfo.cmake_library_path = ["C:\\Users\\lib",
-                                    "C:\\LLVM\\lib\\clang\\9.0.0\\include"]
+    testinfo.cmake_include_path = [r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019\src\main"]
+    testinfo.cmake_library_path = [r"C:\Users\lib",
+                                    r"C:\LLVM\lib\clang\9.0.0\include"]
     return testinfo
 

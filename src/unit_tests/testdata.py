d56cc95a987bc9d8b31dc60982ae2c006deeac78
Jonathan Whitlock <beenencoded@outlook.com>
2020-03-25 22:09:51 -0400

    updated for system I'm using

--- src/unit_tests/testdata.py
+++ src/unit_tests/testdata.py
@@ -6,16 +6,15 @@ logger = logging.getLogger("TEST: " + __name__)
 
 def actual_project_information() -> ProjectInformation:
     testinfo = ProjectInformation()
-    testinfo.project_directory = r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019"
-    testinfo.source_directory = r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019"
-    testinfo.build_directory = r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019\build"
-    testinfo.cpp_compiler = r"C:\LLVM\bin\clang++.exe"
-    testinfo.c_compiler = r"C:\LLVM\bin\clang.exe"
+    testinfo.project_directory = r"D:\beene\Documents\coding\C++\Current_projects\test_2019"
+    testinfo.source_directory = r""
+    testinfo.build_directory = r"build"
+    testinfo.cpp_compiler = r"C:\Program Files\LLVM\bin\clang++.exe"
+    testinfo.c_compiler = r"C:\Program Files\LLVM\bin\clang.exe"
     testinfo.generator_type = SupportedCmakeGenerators.NMAKE_MAKEFILE
     testinfo.cmake_cmd = "cmake.exe"
     testinfo.build_targets = ["debug"]
-    testinfo.cmake_include_path = [r"C:\Users\beene.DESKTOP-NGBJDSG\Documents\coding\C++\Current_projects\test_2019\src\main"]
-    testinfo.cmake_library_path = [r"C:\Users\lib",
-                                    r"C:\LLVM\lib\clang\9.0.0\include"]
+    testinfo.cmake_include_path = [r"D:\beene\Documents\coding\C++\Current_projects\test_2019\src\main"]
+    testinfo.cmake_library_path = [r"C:\Program Files\LLVM\lib\clang\9.0.0\include"]
     return testinfo
 

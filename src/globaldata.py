6a69dbcedcc01695436e6538bc51a0994a3c613e
Jonathan Whitlock <beenencoded@outlook.com>
2020-04-19 11:48:06 -0400

    added log level

--- src/globaldata.py
+++ src/globaldata.py
@@ -1,3 +1,14 @@
+import logging, os
+
 from data import Configuration
 
 CONFIG = Configuration()
+LOG_LEVEL = {
+    "critical": logging.CRITICAL,
+    "error":    logging.ERROR,
+    "warning":  logging.WARNING,
+    "info":     logging.INFO,
+    "debug":    logging.DEBUG,
+    "notset":   logging.NOTSET}[CONFIG['DEFAULT']['loglevel']]
+
+LOGFILE = Configuration.program_home + os.path.sep + "builder.log"
